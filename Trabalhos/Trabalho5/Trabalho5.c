09003af0812c5684f6c68bfd70896358b125e189
brizzigui <brizzigui@gmail.com>
2023-05-18 23:08:02 -0300

    Adiciona todas funcionalidades de rotação, detecção de figura e controle de jogo.

--- Trabalhos/Trabalho5/Trabalho5.c
+++ Trabalhos/Trabalho5/Trabalho5.c
@@ -7,6 +7,9 @@
 
 
 void estado_atual(char matriz[TAM][TAM]){
+
+    printf("\n");
+
     for (int i = 0; i < TAM; i++)
     {
         for (int j = 0; j < TAM; j++)
@@ -67,7 +70,37 @@ void rotaciona_externa(char matriz[TAM][TAM]){
 }
 
 void rotaciona_media(char matriz[TAM][TAM]){
+    char temp, aux;
 
+    aux = matriz[1][1];
+    for (int j = 1; j < TAM-2; j++)
+    {
+        temp = matriz[1][j+1];
+        matriz[1][j+1] = aux;
+        aux = temp;
+    }
+
+    for (int i = 1; i < TAM-2; i++)
+    {
+        temp = matriz[i+1][5];
+        matriz[i+1][5] = aux;
+        aux = temp;
+    }
+    
+    for (int j = TAM-2; j > 1; j--)
+    {
+        temp = matriz[5][j-1];
+        matriz[5][j-1] = aux;
+        aux = temp;
+    }
+
+    for (int i = TAM-2; i > 1; i--)
+    {
+        temp = matriz[i-1][1];
+        matriz[i-1][1] = aux;
+        aux = temp;
+    }
+    
 }
 
 void rotaciona_interna(char matriz[TAM][TAM]){
@@ -104,7 +137,7 @@ void rotaciona_interna(char matriz[TAM][TAM]){
     }
 }
 
-void movimentar(char matriz[TAM][TAM]){
+int movimentar(char matriz[TAM][TAM]){
     
     char escolha = escolha_do_usuario();
 
@@ -119,9 +152,12 @@ void movimentar(char matriz[TAM][TAM]){
         case 'i': case 'I':
             rotaciona_interna(matriz);
             break;
+        case '0':
+            return 0;
     
     }
 
+    return 1;
 
 }
 
@@ -130,35 +166,139 @@ void inicializa_matriz(char matriz[TAM][TAM]){
     {
         for (int j = 0; j < TAM; j++)
         {
-            matriz[i][j] = '.';
+            
+            if(i == j)
+            {
+                matriz[i][j] = 'O';
+            }
+
+            else if(i+j == TAM-1)
+            {
+                matriz[i][j] = 'X';
+            }
+
+            else
+            {
+                matriz[i][j] = '.';
+            }
+            
         }
 
-        matriz[0][0] = 'O';
-        matriz[1][1] = 'X';
-        matriz[0][6] = 'X';
-        matriz[1][5] = 'O';
-        matriz[2][3] = 'O';
-        matriz[3][2] = 'X';
-        matriz[3][4] = 'X';
-        matriz[4][3] = 'O';
-        matriz[5][1] = 'O';
-        matriz[5][5] = 'X';
-        matriz[6][0] = 'X';
-        matriz[6][6] = 'O';
-
-        
     }
+
+    matriz[3][3] = '.';
+
+    for (int i = 0; i < 4; i++)
+    {
+        rotaciona_media(matriz);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        rotaciona_interna(matriz);
+    }
+    
+
     
 }
 
+char checa_figura(char matriz[TAM][TAM]){
+    
+    //cruz
+    for (int j = 0; j < TAM && matriz[3][j] == 'X'; j++)
+    {
+        if(j == 2)
+        {
+            j++;
+        }
+
+        else if(j == TAM-1){
+            return 'c';
+        }   
+            
+    }
+
+    //cruz
+    for (int j = 0; j < TAM && matriz[3][j] == 'O'; j++)
+    {
+        if(j == 2)
+        {
+            j++;
+        }
+
+        else if(j == TAM-1){
+            return 'c';
+        }   
+            
+    }
+
+    //x
+    for (int i= 0, j = 0; i < TAM && matriz[i][j] == 'X'; i++, j++)
+    {
+        if(i == 2)
+        {
+            i++;
+            j++;
+        }
+
+        else if(j == TAM-1){
+            return 'x';
+        }   
+            
+    }
+
+    //x
+    for (int i= 0, j = 0; i < TAM && matriz[i][j] == 'O'; i++, j++)
+    {
+        if(i == 2)
+        {
+            i++;
+            j++;
+        }
+
+        else if(j == TAM-1){
+            return 'x';
+        }   
+            
+    }
+
+    return '0';
+
+
+
+
+}
 
 int main(){
     char matriz[TAM][TAM];
     inicializa_matriz(matriz);
-    estado_atual(matriz);
-    while(1){
-        movimentar(matriz);
-        estado_atual(matriz);
-    }
     
+    char figura;
+    int continuar;
+
+
+    do
+    {
+        estado_atual(matriz);
+        figura = checa_figura(matriz);
+        
+        
+        if (figura == 'c')
+        {
+            printf("CRUZ FORMADA NA TELA!\n\n");
+        }
+
+        else if(figura == 'x')
+        {
+            printf("X FORMADO NA TELA!\n\n");
+        }
+
+        continuar = movimentar(matriz);
+        system("cls || clear");
+
+    }while(continuar != 0);
+
+    printf("Obrigado por jogar!\n");
+    printf("Por Guilherme Brizzi\n");
+
 }
