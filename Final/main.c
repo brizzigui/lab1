8c9599da17c0ca3d83cdb271f32fd2d0b20f8f79
brizzigui <brizzigui@gmail.com>
2023-06-09 00:16:06 -0300

    Corrige bug na lógica dos cliques

--- Final/main.c
+++ Final/main.c
@@ -38,7 +38,7 @@ void inicializar()
     al_init_ttf_addon();
 }
 
-int botao_padrao(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int *click)
+int botao_padrao(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int click)
 {
     float x0 = x;
     float x1 = x0 + LARG_BOTAO_PADRAO;
@@ -46,16 +46,16 @@ int botao_padrao(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, floa
     float y1 = y0 + ALT_BOTAO_PADRAO;
 
     al_draw_bitmap(imagem_botao, x0, y0, 0);
-    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && *click)
+    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && click)
     {
-        *click = 0;
+        click = 0;
         return 1;
     }
 
     return 0;
 }
 
-int botao_pequeno(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int *click)
+int botao_pequeno(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int click)
 {
     float x0 = x;
     float x1 = x0 + LARG_BOTAO_PEQ;
@@ -63,9 +63,9 @@ int botao_pequeno(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, flo
     float y1 = y0 + ALT_BOTAO_PEQ;
 
     al_draw_bitmap(imagem_botao, x0, y0, 0);
-    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && *click)
+    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && click)
     {
-        *click = 0;
+        click = 0;
         return 1;
     }
 
@@ -119,11 +119,11 @@ char menu(ALLEGRO_DISPLAY* display)
         al_clear_to_color(AZUL_ESCURO);
         al_draw_text(font_2p_regular_72, BRANCO, LARGURA_TELA/2, ALTURA_TELA/4, ALLEGRO_ALIGN_CENTER, "DecaDado");
 
-        bool ir_jogar = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO/2, 450, botao_jogar, x_mouse, y_mouse, &click);
-        bool ir_restaurar = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO-OFFSET_BOTAO_PADRAO_X, 450+ALT_BOTAO_PADRAO+OFFSET_BOTAO_PADRAO_Y, botao_restaurar, x_mouse, y_mouse, &click);
-        bool ir_record = botao_padrao(LARGURA_TELA/2+OFFSET_BOTAO_PADRAO_X, 450+ALT_BOTAO_PADRAO+OFFSET_BOTAO_PADRAO_Y, botao_record, x_mouse, y_mouse, &click);
-        bool ir_help = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO-OFFSET_BOTAO_PADRAO_X, 450+(OFFSET_BOTAO_PADRAO_Y+ALT_BOTAO_PADRAO)*2, botao_help, x_mouse, y_mouse, &click);
-        bool ir_sair = botao_padrao(LARGURA_TELA/2+OFFSET_BOTAO_PADRAO_X, 450+(OFFSET_BOTAO_PADRAO_Y+ALT_BOTAO_PADRAO)*2, botao_sair, x_mouse, y_mouse, &click);
+        bool ir_jogar = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO/2, 450, botao_jogar, x_mouse, y_mouse, click);
+        bool ir_restaurar = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO-OFFSET_BOTAO_PADRAO_X, 450+ALT_BOTAO_PADRAO+OFFSET_BOTAO_PADRAO_Y, botao_restaurar, x_mouse, y_mouse, click);
+        bool ir_record = botao_padrao(LARGURA_TELA/2+OFFSET_BOTAO_PADRAO_X, 450+ALT_BOTAO_PADRAO+OFFSET_BOTAO_PADRAO_Y, botao_record, x_mouse, y_mouse, click);
+        bool ir_help = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO-OFFSET_BOTAO_PADRAO_X, 450+(OFFSET_BOTAO_PADRAO_Y+ALT_BOTAO_PADRAO)*2, botao_help, x_mouse, y_mouse, click);
+        bool ir_sair = botao_padrao(LARGURA_TELA/2+OFFSET_BOTAO_PADRAO_X, 450+(OFFSET_BOTAO_PADRAO_Y+ALT_BOTAO_PADRAO)*2, botao_sair, x_mouse, y_mouse, click);
 
 
         if(ir_jogar)
@@ -142,6 +142,8 @@ char menu(ALLEGRO_DISPLAY* display)
             al_flip_display();
             update = false;
         }
+
+        click = 0;
     }
 }
 
@@ -197,7 +199,7 @@ void jogo(ALLEGRO_DISPLAY* display, int restaura)
         al_draw_bitmap(matriz_background, 0, 0, 0);
             
 
-        bool pause_click = botao_padrao(LARGURA_TELA-LARG_BOTAO_PADRAO-OFFSET_BOTAO_PADRAO_X, OFFSET_BOTAO_PADRAO_Y, botao_sair, x_mouse, y_mouse, &click);
+        bool pause_click = botao_padrao(LARGURA_TELA-LARG_BOTAO_PADRAO-OFFSET_BOTAO_PADRAO_X, OFFSET_BOTAO_PADRAO_Y, botao_sair, x_mouse, y_mouse, click);
 
         if (pause_click)
         {
@@ -208,9 +210,9 @@ void jogo(ALLEGRO_DISPLAY* display, int restaura)
         {
             al_draw_filled_rectangle(0, 0, LARGURA_TELA, ALTURA_TELA, al_map_rgba(0, 0, 0, 200));
             al_draw_text(font_2p_regular_72, BRANCO, LARGURA_TELA/2, ALTURA_TELA/4, ALLEGRO_ALIGN_CENTER, "Paused");
-            bool sair_salvando = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO/2, ALTURA_TELA/3+OFFSET_BOTAO_PADRAO_Y, botao_salvar_e_sair, x_mouse, y_mouse, &click);
-            bool sair_sem_salvar = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO/2, ALTURA_TELA/3+OFFSET_BOTAO_PADRAO_Y*2+ALT_BOTAO_PADRAO, botao_sair, x_mouse, y_mouse, &click);
-            bool voltar = botao_pequeno(LARGURA_TELA/2-LARG_BOTAO_PEQ/2, ALTURA_TELA/3+OFFSET_BOTAO_PADRAO_Y*3+ALT_BOTAO_PADRAO*2, botao_voltar, x_mouse, y_mouse, &click);
+            bool sair_salvando = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO/2, ALTURA_TELA/3+OFFSET_BOTAO_PADRAO_Y, botao_salvar_e_sair, x_mouse, y_mouse, click);
+            bool sair_sem_salvar = botao_padrao(LARGURA_TELA/2-LARG_BOTAO_PADRAO/2, ALTURA_TELA/3+OFFSET_BOTAO_PADRAO_Y*2+ALT_BOTAO_PADRAO, botao_sair, x_mouse, y_mouse, click);
+            bool voltar = botao_pequeno(LARGURA_TELA/2-LARG_BOTAO_PEQ/2, ALTURA_TELA/3+OFFSET_BOTAO_PADRAO_Y*3+ALT_BOTAO_PADRAO*2, botao_voltar, x_mouse, y_mouse, click);
              
             if (sair_salvando)
             {
@@ -234,7 +236,9 @@ void jogo(ALLEGRO_DISPLAY* display, int restaura)
         {
             al_flip_display();
             update = false;
-        }   
+        }
+
+        click = 0;   
     }
 }
 
@@ -309,10 +313,10 @@ void help(ALLEGRO_DISPLAY* display)
         sprintf(contador, "Pág. %d/4", pagina);
         al_draw_text(font_2p_regular_24, BRANCO, LARGURA_TELA-LARG_BOTAO_PEQ-OFFSET_BOTAO_PEQ_X, ALTURA_TELA-ALT_BOTAO_PEQ-OFFSET_BOTAO_PEQ_Y*6, 0, contador);
 
-        bool voltar = botao_pequeno(OFFSET_BOTAO_PEQ_X, ALTURA_TELA-ALT_BOTAO_PEQ-OFFSET_BOTAO_PEQ_Y, botao_voltar, x_mouse, y_mouse, &click);
+        bool voltar = botao_pequeno(OFFSET_BOTAO_PEQ_X, ALTURA_TELA-ALT_BOTAO_PEQ-OFFSET_BOTAO_PEQ_Y, botao_voltar, x_mouse, y_mouse, click);
         bool next_page = 0;
         if (pagina < 4)
-            next_page = botao_pequeno(LARGURA_TELA-OFFSET_BOTAO_PEQ_X-LARG_BOTAO_PEQ, ALTURA_TELA-ALT_BOTAO_PEQ-OFFSET_BOTAO_PEQ_Y, prox_pag, x_mouse, y_mouse, &click);
+            next_page = botao_pequeno(LARGURA_TELA-OFFSET_BOTAO_PEQ_X-LARG_BOTAO_PEQ, ALTURA_TELA-ALT_BOTAO_PEQ-OFFSET_BOTAO_PEQ_Y, prox_pag, x_mouse, y_mouse, click);
 
         if(voltar)
         {
@@ -334,6 +338,7 @@ void help(ALLEGRO_DISPLAY* display)
             update = false;
         }
         
+        click = 0;
     }
 }
 
