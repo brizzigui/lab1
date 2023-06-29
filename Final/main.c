70ee126d795a583812dd96c17e1748db9b86ce41
brizzigui <brizzigui@gmail.com>
2023-06-29 15:28:39 -0300

    Desfaz divisão em dois arquivos

--- Final/main.c
+++ Final/main.c
@@ -62,8 +62,6 @@
 #define CODE_GREEN 3
 #define CODE_PURPLE 4
 
-#include "code/buttons.h"
-
 void inicializar()
 {
     al_init();
@@ -73,6 +71,57 @@ void inicializar()
     al_init_font_addon();
     al_init_image_addon();
     al_init_ttf_addon();
+}
+
+int botao_padrao(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int click)
+{
+    float x0 = x;
+    float x1 = x0 + LARG_BOTAO_PADRAO;
+    float y0 = y;
+    float y1 = y0 + ALT_BOTAO_PADRAO;
+
+    al_draw_bitmap(imagem_botao, x0, y0, 0);
+    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && click)
+    {
+        click = 0;
+        return 1;
+    }
+
+    return 0;
+}
+
+int botao_pequeno(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int click)
+{
+    float x0 = x;
+    float x1 = x0 + LARG_BOTAO_PEQ;
+    float y0 = y;
+    float y1 = y0 + ALT_BOTAO_PEQ;
+
+    al_draw_bitmap(imagem_botao, x0, y0, 0);
+    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && click)
+    {
+        click = 0;
+        return 1;
+    }
+
+    return 0;
+}
+
+int botao_mini(int x, int y, ALLEGRO_BITMAP* imagem_botao, float x_mouse, float y_mouse, int click)
+{
+    float x0 = x;
+    float x1 = x0 + LARG_BOTAO_MINI;
+    float y0 = y;
+    float y1 = y0 + ALT_BOTAO_MINI;
+
+    al_draw_bitmap(imagem_botao, x0, y0, 0);
+    if (x_mouse >= x0 && x_mouse <= x1 && y_mouse >= y0 && y_mouse <= y1 && click)
+    {
+        click = 0;
+        return 1;
+    }
+
+    return 0;
 }
 
 char menu(ALLEGRO_DISPLAY* display)
